3c41045fb0e192dca119ffd511fb40ef65897f4b
land <zywei_1988@163.com>
2020-04-12 12:20:49 +0800

    ceshi

--- childMavenProject/src/main/java/TestHutool.java
+++ childMavenProject/src/main/java/TestHutool.java
@@ -3,7 +3,7 @@ import java.util.Date;
 import cn.hutool.core.date.DateUtil;
 public class TestHutool {
     public static void main(String args[]){
-        String dateStr = "2012-12-12 12:12:12";
+        String dateStr = "2020-4-12 12:12:12";
         Date date = DateUtil.parse(dateStr);
         System.out.println(date);
 
